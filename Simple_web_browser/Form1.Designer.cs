e5fb03d22b102331f49bde8a0e183033748fe80d
krikjers <krikjers@stud.ntnu.no>
2017-04-16 19:21:35 +0200

    Anchored textbox and button

--- Simple_web_browser/Form1.Designer.cs
+++ Simple_web_browser/Form1.Designer.cs
@@ -34,7 +34,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(105, 128);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(197, 226);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -46,9 +47,12 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(260, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(260, 208);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
