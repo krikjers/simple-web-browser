19e2a6bcb02bd492840224290f6d9e01b23fa518
krikjers <krikjers@stud.ntnu.no>
2017-04-16 19:23:04 +0200

    Deleted testing

--- Simple_web_browser/Form1.Designer.cs
+++ Simple_web_browser/Form1.Designer.cs
@@ -28,41 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(197, 226);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
-            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(260, 208);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "b";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -71,9 +43,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Simple_web_browser/Form1.cs
+++ Simple_web_browser/Form1.cs
@@ -23,37 +23,4 @@ namespace Simple_web_browser
         }
 
 
-        //to create these functions below: select button in Form1.cs [design] and select events tab (a lightning) and double click on an event
-
-        /// <summary>
-        /// When button1 is clicked, run this code
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_Click(object sender, EventArgs e)
-        {
-            richTextBox1.AppendText("A button was clicked\r\n");        // \r\n Used as a new line character in Windows
-        }
-
-
-        /// <summary>
-        /// run this code when mouse is inside button1 perimeter
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_MouseEnter(object sender, EventArgs e)
-        {
-            richTextBox1.AppendText("the mouse has entered the button\r\n");
-        }
-
-        /// <summary>
-        /// run code when the mouse leaves the button perimeter
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_MouseLeave(object sender, EventArgs e)
-        {
-            richTextBox1.AppendText("the mouse leaves the button\r\n");
-        }
-    }
 }
